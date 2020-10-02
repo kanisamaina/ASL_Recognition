438a4bad3c67d669ce2ccee6e5ce3f34c8dd67a5
Kanishka <55542796+kanisamaina@users.noreply.github.com>
2020-10-02 09:27:12 +0530

    Update ASL.py

--- ASL.py
+++ ASL.py
@@ -2,12 +2,12 @@ from tensorflow.keras.models import model_from_json
 
 import numpy as np
 
-json_file = open("3_alphabet_ASL_Model.json", "r")
+json_file = open("alphabet_ASL_Model.json", "r")
 loaded_json_model = json_file.read()
 json_file.close()
 
 model = model_from_json(loaded_json_model)
-model.load_weights("3_alphabet_ASL_Model_weights.h5")
+model.load_weights("alphabet_ASL_Model_weights.h5")
 
 labels = list("ABCDEFGHIJ")
 
